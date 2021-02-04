c4eea2176f821c28867ee25bee2e2cc53f9e3cee
jiessie <1824748149@qq.com>
2021-02-04 10:47:08 +0800

    第二次提交创建测试代码

--- demo/src/com/duyi/TestMain.java
+++ demo/src/com/duyi/TestMain.java
@@ -1,7 +1,7 @@
 package com.duyi;
 
-public class Test {
+public class TestMain {
     public static void main(String[] args) {
-        System.out.println("测试代码");
+        System.out.println("第二次修改");
     }
 }
